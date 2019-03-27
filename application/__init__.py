7078888ce32c00253af4eeb64443e86708e0fa85
porrasm <31691452+porrasm@users.noreply.github.com>
2019-03-27 17:44:04 +0200

    add react, add react files

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 
 from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
